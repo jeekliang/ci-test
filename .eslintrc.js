ced6a8bf75c8a2692b1260f6565cb6633cd07271
jxliang <jeekliang@163.com>
2024-08-30 14:52:35 +0800

    chore: 项目配置更新：ESLint、Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-  extends: require.resolve('@umijs/max/eslint'),
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended', // 使用推荐的TypeScript规则
+    'plugin:prettier/recommended', // 推荐的Prettier规则
+  ],
+  rules: {
+    // 可以在这里覆盖某些规则
+  },
 };
